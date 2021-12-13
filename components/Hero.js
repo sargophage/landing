dd9625646725e5da3414ef71c4638c3d0d25a6a8
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-12-13 08:49:54 -0400

    lien vers le formulaire

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
                 <span className="text-primary font-weight-medium"> Sargassum</span></h1>
               <p className="text-muted mb-4 pb-2">Always know what's happening on the beaches with Sargowaze. Our prevision app, powered by the community is a tool to measure, monitor, forecast and suport
                 the handling of Sargassum beaching. Wanna help?</p>
-              <a href="#" className="btn btn-warning">
+              <a href="https://app.evalandgo.com/s/index.php?id=JTk5bCU5NG0lOUElQjI%3D&a=JTk3cSU5QW8lOUYlQUQ%3D" className="btn btn-warning">
                 Find Out How <span className="ml-2 right-icon">&#8594;</span>
               </a>
             </div>
